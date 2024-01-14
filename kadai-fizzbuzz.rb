38b213a7badc2037f2334dd7a2ffc4030c6e4dd3
takehiro.nakai <tn.44.formal@gmail.com>
2024-01-14 14:36:16 +0000

    kadai-ruby-2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,20 +1,19 @@
 
 
 # 特定の値に対して処理を行うメソッドFizzBuzzを定義
-def fizzbuzz(a,b)
-    (a..b).each do |number|
+def fizzbuzz(number)
   if number % 15 == 0
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elsif number % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif number % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   else 
-    puts number.to_s
+    return number.to_s
   end
-end
 end
 # 1からnum_maxまでのループでFizzBuzzを実行
  num_max = 100
-puts fizzbuzz(1,num_max)
-return 
+ (1..num_max).each do |number|
+puts fizzbuzz(number)
+end
